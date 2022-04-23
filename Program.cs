adfeee7fdfa1b01da780d83832f2a7693142535b
Joshua Smith <jgs73@live.co.uk>
2022-04-23 17:57:52 +0100

    Initial stages of figuring out method to store questions and answers - Currently a split response of the question and answers are written to a text file - TODO: Create an object to store a question and answer(s) - TODO: Serialise question and answers in JSON or XML format? (format TBD) - TODO: Distinguish between Gamesmaster and Player?

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace QuizMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a question for the bank, with answers separated by | (Mark the correct answer with a '*' at the end)");
+            string response = Console.ReadLine();
+            StreamWriter writer;
+            string[] response_array = response.Split("|");
+            foreach (string str in response_array) {
+                using (writer = new StreamWriter("question_and_answer_bank.txt"))
+                {
+                    writer.WriteLine(str);
+                }
+                writer.Close();
+            }
+        }
+    }
+}
---
+++ QAndABank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMachine
+{
+    internal class QAndABank
+    {
+        List<string> questions = new List<string>();
+        string[] answers;
+
+        public void storeQuestionAndAnswers(string response)
+        {
+            string[] vs = response.Split('|');
+            foreach (string v in vs)
+            {
+                if (v.Contains("?")){
+                    questions.Add(v);
+                }
+                answers[0] = v;
+            }
+        }
+    }
+}
