48feed4717d69a086e2454366647ee005d96d83d
Joshua Smith <jgs73@live.co.uk>
2022-04-28 22:00:45 +0100

    Created methods to write to file and read from file - TODO: use `QAndABank` class to serialise/de-serialise XML in next commit

--- Program.cs
+++ Program.cs
@@ -9,7 +9,36 @@ namespace QuizMachine
             QAndABank qaBank = new QAndABank();
             Console.WriteLine("Enter a question for the bank, with answers separated by | (Mark the correct answer with a '*' at the end)");
             string response = Console.ReadLine();
-            qaBank.storeQuestionAndAnswers(response);
+
+            SaveQuestionBank(response);
+        }
+
+        public static void SaveQuestionBank(string response)
+        {
+            StreamWriter writer;
+            string[] vs = response.Split('|');
+            using (writer = new StreamWriter("question_and_answer_bank.txt"))
+            {
+                foreach (string str in vs)
+                {
+                    writer.WriteLine(str);
+                }
+            }
+            writer.Close();
+        }
+
+        public static String ReadQuestionBank()
+        {
+            using (StreamReader reader = new StreamReader("question_and_answer_bank.txt"))
+            {
+                for (int i = 0;  i < reader.ReadToEnd().Length; i++)
+                if (reader.ToString().Contains("?"))
+                {
+                    QAndABank.questions.Add(reader.ToString());
+                    continue;
+                }
+            }
+            return ""; // Think I need to return an object here, will reassess in next commit
         }
     }
 }
--- QAndABank.cs
+++ QAndABank.cs
@@ -8,27 +8,8 @@ namespace QuizMachine
 {
     internal class QAndABank
     {
-        List<string> questions = new List<string>();
-        string[] answers;
-        StreamWriter writer;
-
-        public void storeQuestionAndAnswers(string response)
-        {
-            string[] vs = response.Split('|');
-            foreach (string str in vs)
-            {
-                using (writer = new StreamWriter("question_and_answer_bank.txt"))
-                {
-                    if (str.Contains("?"))
-                    {
-                        questions.Add(str);
-                        continue;
-                    }
-                    Console.WriteLine(str);
-                    writer.WriteLine(str);
-                }
-            }
-            writer.Close();
-        }
+        public static List<string> questions = new List<string>();
+        public string[] answers;
+        public int correctAnswerIndex;
     }
 }
